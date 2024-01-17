6e11241bed5e962f0b7f8ee7d8d5a04bdd778c62
jisooooooooooo <dlawltn0708@naver.com>
2024-01-17 20:33:04 +0900

    component style files

--- Section3/src/App.jsx
+++ Section3/src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import { CORE_CONCEPTS } from "./data";
 import CoreConcept from "./components/CoreConcept";
 
