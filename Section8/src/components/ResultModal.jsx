6b15157feac28dad84150f729dde5cbcc2c4b6d1
jisooooooooooo <dlawltn0708@naver.com>
2024-02-01 13:05:21 +0900

    improve the resulting modal window

--- Section8/src/components/ResultModal.jsx
+++ Section8/src/components/ResultModal.jsx
@@ -5,8 +5,10 @@ const ResultModal = forwardRef(function ResultModal(
   ref
 ) {
   const dialog = useRef();
+
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -19,6 +21,7 @@ const ResultModal = forwardRef(function ResultModal(
   return (
     <dialog ref={dialog} className="result-modal" open>
       {userLost && <h2>You Lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
