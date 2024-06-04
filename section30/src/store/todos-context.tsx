d4cc10ae91464567d360815b96eb8f7090960bc7
jisooooooooooo <dlawltn0708@naver.com>
2024-06-04 13:39:35 +0900

    typescript

---
+++ section30/src/store/todos-context.tsx
@@ -0,0 +1,51 @@
+import React, { useState, ReactNode } from "react";
+import Todo from "../models/todo";
+
+type TodosContextObj = {
+  items: Todo[];
+  addTodo: (text: string) => void;
+  removeTodo: (id: string) => void;
+};
+
+export const TodosContext = React.createContext<TodosContextObj>({
+  items: [],
+  addTodo: () => {},
+  removeTodo: (id: string) => {},
+});
+
+// 컨텍스트의 상태를 관리하는 역할
+interface Props {
+  children: ReactNode;
+}
+
+const TodosContextProvider: React.FC<Props> = ({ children }) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
+  const contextValue: TodosContextObj = {
+    items: todos,
+    addTodo: addTodoHandler,
+    removeTodo: removeTodoHandler,
+  };
+
+  return (
+    <TodosContext.Provider value={contextValue}>
+      {children}
+    </TodosContext.Provider>
+  );
+};
+
+export default TodosContextProvider;
